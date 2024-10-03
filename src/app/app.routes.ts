81eec4ba54b188fdbe310d102a9fedd2f099216c
davidyujia <davidyujia@gmail.com>
2024-10-03 19:16:53 +0800

    add @angular/material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: []
+  },
+  {
+    path: 'Home',
+    component: HomeComponent,
+    children: []
+  },
+  {
+    path: 'Home2',
+    component: HomeComponent,
+    children: []
+  },
+  {
+    path: '**',
+    title: 'Page Not Found',
+    component: PageNotFoundComponent
+  }
+];
